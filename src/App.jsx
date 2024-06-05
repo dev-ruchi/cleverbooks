6d27b07fea1171c01cf579ce06dd17a7eeaf0977
Ruchi <ruchi.thanabhawan@gmail.com>
2024-06-05 14:33:10 -0700

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import KeyProblemsSection from "./components/KeyProblemsSection";
+import KeyMatricsSection from "./components/KeyMatricsSection";
+import ReviewsSection from "./components/ReviewsSection";
+import ProblemTargetSection from "./components/ProblemTargetSection";
+import CTASection from "./components/CTASection";
+import FooterSection from "./components/FooterSection";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <KeyProblemsSection />
+      <KeyMatricsSection />
+      <ReviewsSection />
+      <ProblemTargetSection />
+      <CTASection />
+      <FooterSection />
+    </>
+  );
+}
+
+export default App;
