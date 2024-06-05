341b22875426caf8d6c5a9f7bed42e09cc6497b5
Ruchi <ruchi.thanabhawan@gmail.com>
2024-06-05 14:32:47 -0700

    add footer section

---
+++ src/components/FooterSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FooterSection = () => {
+  return (
+    <div>
+      <footer className="bg-gray-800 text-white py-6">
+        <div className="container mx-auto text-center">
+          <p>&copy; 2024 CleverBooks. All rights reserved.</p>
+          <p>123 CleverBooks Lane, Booktown, BK 12345</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default FooterSection;
